102d8603f8f9d762030482b33fa12716754be121
Adrian Kryzinski <adriankryzinski@gmail.com>
2021-04-23 19:05:41 +0200

    Preparation of the project structure

---
+++ react-router-project/src/layouts/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div>React Router Project</div>;
+  }
+}
+
+export default App;
