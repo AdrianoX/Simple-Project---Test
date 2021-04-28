42d8dbacb6ae50b994d7e5dd3a7f035ec16ee4a4
Adrian Kryzinski <adriankryzinski@gmail.com>
2021-04-28 18:13:45 +0200

    Work on menu

--- react-router-project/src/layouts/Navigation.js
+++ react-router-project/src/layouts/Navigation.js
@@ -1,10 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-class Navigation extends Component {
-  state = {};
-  render() {
-    return <div>Navigation</div>;
-  }
-}
+const list = [
+  { name: "start", path: "/" },
+  { name: "products", path: "/products" },
+  { name: "contact", path: "/contact" },
+  { name: "admin panel", path: "/admin" },
+];
+
+const Navigation = () => {
+  const menu = list.map((item) => (
+    <li key={item.name}>
+      <NavLink to={item.path}>{item.name}</NavLink>
+    </li>
+  ));
+
+  return (
+    <nav>
+      <ul>
+        {/* <li>
+          <NavLink to="/">Start</NavLink>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <NavLink to="Products">Products</NavLink>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <NavLink to="contact">Contacts</NavLink>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <NavLink to="admin">Admin Panel</NavLink>
+        </li> */}
+        {menu}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
